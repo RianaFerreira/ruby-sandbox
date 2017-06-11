3c25bb6f2f883b6d17cdef320c09e061eeef6364
Riana Ferreira <rianaferreira@gmail.com>
2017-06-11 18:56:40 +1000

    Complex Ruby data structures

--- arrays.rb
+++ arrays.rb
@@ -1,13 +1,22 @@
-# array used for the collection of data
+# array used for the collection of data in a set order
 meals = ["breakfast","lunch","dinner"]
 
 # append an item to the end of the array
 meals << "dessert" # => ["breakfast","lunch","dinner","dessert"]
 
-meals[2] # => "dinner"
-meals.last # => "dessert"
+puts meals[2]   # => "dinner"
+puts meals.last # => "breakfast"
+puts meals.last # => "dessert"
 
 # sort
 one = ["this","is","an","array"]
-one.sort # => ["an","array","this","is"]
-one
+two = ["another","example","of","an","array"]
+puts one.sort # => ["an","array","this","is"]
+puts one
+puts one.join              # => thisisanarray
+puts one.join("-")         # => this-is-an-array
+puts one.index("is")       # => 1
+puts one.include?("hello") # => false
+one.each{ |el| puts el }
+puts one.collect { |el| el + "1" }
+puts one.collect
